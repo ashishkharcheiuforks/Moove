c53cd5d1a5e454d09483056bb53132a5e8f96bc3
Nazar Suhovich <n.suhovich@gmail.com>
2019-02-15 10:06:17 +0200

    Start work on new design.

--- mobile/src/main/java/com/backdoor/moove/modern_ui/HomeActivity.kt
+++ mobile/src/main/java/com/backdoor/moove/modern_ui/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.backdoor.moove.modern_ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.backdoor.moove.R
@@ -9,5 +10,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navHost?.let { navFragment ->
+            navFragment.childFragmentManager.primaryNavigationFragment?.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
